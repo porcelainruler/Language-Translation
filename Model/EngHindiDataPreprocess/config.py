747f9d396c2ce11c1f074163b459e01081318f57
Shubham <princeshubham22699@gmail.com>
2020-07-12 21:36:30 +0530

    ENG-HIN Data Preprocess Update - Both Language Vocab Creation, Tokenization and <Word Token> to <Int Token> Done

---
+++ Model/EngHindiDataPreprocess/config.py
@@ -0,0 +1,31 @@
+# Just for not producing error Need to Remove SRC & TRG with corr. value during Data Pre-Processing:
+
+# INPUT_DIM = len(SRC.vocab)      # Not used here value to be modified
+# OUTPUT_DIM = len(TRG.vocab)     # Not used here value to be modified
+EMB_DIM = 256
+HID_DIM = 512  # each conv. layer has 2 * hid_dim filters
+ENC_LAYERS = 7  # number of conv. blocks / layers in encoder
+DEC_LAYERS = 7  # number of conv. blocks / layers in decoder
+ENC_KERNEL_SIZE = 3  # must be odd!
+DEC_KERNEL_SIZE = 3  # can be even or odd
+ENC_DROPOUT = 0.25
+DEC_DROPOUT = 0.25
+BATCH_SIZE = 64
+
+MAX_LENGTH = 61
+
+# Model Train Parameters
+EPOCH = 150
+Learning_Rate = 0.001
+
+# Setting up special Tokens
+SOS_TOKEN = '<sos>'
+EOS_TOKEN = '<eos>'
+PAD_TOKEN = '<pad>'
+UNK_TOKEN = '<unk>'
+
+# Vector value of Special Tokens
+SOS_TOKEN_IDX = 1
+EOS_TOKEN_IDX = 2
+PAD_TOKEN_IDX = 0
+UNK_TOKEN_IDX = 3
---
+++ Model/EngHindiDataPreprocess/data-preprocess.py
@@ -0,0 +1,144 @@
+import gzip
+import codecs
+import math
+from Model.EngHindiDataPreprocess import config
+
+
+def load_data_sp(path):
+    with codecs.open(path, encoding='utf-8') as f:
+        data = f.read().split('\n')
+        print('Num of Lines in Data:', len(data))
+
+    return data
+
+
+def tokenizer(data: list, flag: bool = True, max_length: int = math.inf):
+    assert type(data) == list, 'Raw Data should be in list data type'
+    print(max_length)
+
+    token_list = list()
+    for line in data:
+        if flag:
+            tokens = ['<sos>'] + line.split(' ') + ['<eos>']
+        else:
+            tokens = line.split(' ')
+
+        token_list.append(tokens)
+
+        if len(token_list) > max_length:
+            break
+    return token_list
+
+
+# Vocab Code for Hindi
+hin_vocab_intToText = dict()
+hin_vocab_textToInt = dict()
+
+# Vocab Code for English
+eng_vocab_intToText = dict()
+eng_vocab_textToInt = dict()
+
+# Setting up some Special Token value for Hindi Vocab
+hin_vocab_textToInt[config.SOS_TOKEN] = config.SOS_TOKEN_IDX
+hin_vocab_textToInt[config.EOS_TOKEN] = config.EOS_TOKEN_IDX
+hin_vocab_textToInt[config.PAD_TOKEN] = config.PAD_TOKEN_IDX
+hin_vocab_textToInt[config.UNK_TOKEN] = config.UNK_TOKEN_IDX
+
+hin_vocab_intToText[config.SOS_TOKEN_IDX] = config.SOS_TOKEN
+hin_vocab_intToText[config.EOS_TOKEN_IDX] = config.EOS_TOKEN
+hin_vocab_intToText[config.PAD_TOKEN_IDX] = config.PAD_TOKEN
+hin_vocab_intToText[config.UNK_TOKEN_IDX] = config.UNK_TOKEN
+
+# Setting up some Special Token value for Hindi Vocab
+eng_vocab_textToInt[config.SOS_TOKEN] = config.SOS_TOKEN_IDX
+eng_vocab_textToInt[config.EOS_TOKEN] = config.EOS_TOKEN_IDX
+eng_vocab_textToInt[config.PAD_TOKEN] = config.PAD_TOKEN_IDX
+eng_vocab_textToInt[config.UNK_TOKEN] = config.UNK_TOKEN_IDX
+
+eng_vocab_intToText[config.SOS_TOKEN_IDX] = config.SOS_TOKEN
+eng_vocab_intToText[config.EOS_TOKEN_IDX] = config.EOS_TOKEN
+eng_vocab_intToText[config.PAD_TOKEN_IDX] = config.PAD_TOKEN
+eng_vocab_intToText[config.UNK_TOKEN_IDX] = config.UNK_TOKEN
+
+count = 4
+
+
+def create_hindi_vocab(data: list):
+    global count
+
+    for arr in data:
+        for token in arr:
+            if token in hin_vocab_textToInt.keys():
+                continue
+            else:
+                hin_vocab_textToInt[token] = count
+                hin_vocab_intToText[count] = token
+                count += 1
+
+    return hin_vocab_textToInt, hin_vocab_intToText
+
+
+def create_eng_vocab(data: list):
+    global count
+
+    for arr in data:
+        for token in arr:
+            if token in eng_vocab_textToInt.keys():
+                continue
+            else:
+                eng_vocab_textToInt[token] = count
+                eng_vocab_intToText[count] = token
+                count += 1
+
+    return eng_vocab_textToInt, eng_vocab_intToText
+
+
+def convert_seq_to_int(data: list, flag: bool):
+    arr = list()
+    for line in data:
+        tok_line = list()
+        for token in line:
+            if flag:
+                if token in eng_vocab_textToInt.keys():
+                    tok_line.append(eng_vocab_textToInt[token])
+                else:
+                    tok_line.append(eng_vocab_textToInt['<unk>'])
+            else:
+                if token in hin_vocab_textToInt.keys():
+                    tok_line.append(hin_vocab_textToInt[token])
+                else:
+                    tok_line.append(hin_vocab_textToInt['<unk>'])
+
+        arr.append(tok_line)
+
+    return arr
+
+
+# hin_vocab_read = load_data_sp('monolingual.hi')
+hin_read = load_data_sp('IITB.en-hi.hi')
+HIN_TOKEN_FORM = tokenizer(hin_read, flag=True, max_length=50)
+create_hindi_vocab(HIN_TOKEN_FORM)
+
+
+eng_read = load_data_sp('IITB.en-hi.en')
+ENG_TOKEN_FORM = tokenizer(eng_read, flag=True, max_length=50)
+create_eng_vocab(ENG_TOKEN_FORM)
+
+
+print(eng_vocab_textToInt)
+print(hin_vocab_textToInt)
+
+ENG_DATA = convert_seq_to_int(ENG_TOKEN_FORM, flag=True)
+HIN_DATA = convert_seq_to_int(HIN_TOKEN_FORM, flag=False)
+
+print(ENG_DATA)
+print(HIN_DATA)
+
+# ENG = tokenizer(eng_read, max_length=50)
+# HIN = tokenizer(hin_read, max_length=50)
+
+
+# w1 = HIN[0][1]
+# w2 = HIN[0][1]
+
+# print(w1, w2, (w1==w2))
